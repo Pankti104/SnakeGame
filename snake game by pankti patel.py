f6eab746a0cf464bf5c60d663c7c119e3cfcf751
Pankti104 <55565466+Pankti104@users.noreply.github.com>
2022-12-02 17:32:45 -0500

    Add files via upload

---
+++ snake game by pankti patel.py
@@ -0,0 +1,249 @@
+#explain workflow of the model by presentation
+#change the dimension of pixel of the window to the maximum screen pixels of your pc/laptop
+#change the turtle , background color
+#make attractive background
+# change the rules for border of the snake
+# change the rule of game so that if snake touch it's body still the game should not terminate.
+#change keys on keyboard. game should usee arrow keys for the snake
+# Also solve wonderful challenge given by Aryan.
+# Add another point where if the snake touches it lose 10 points
+# Change the font and its size
+#Have a happy homework day:)
+#--Vrushang
+
+import turtle
+import time
+import random
+from win32api import GetSystemMetrics
+
+delay = 0.1
+width = GetSystemMetrics(0)
+height = GetSystemMetrics(1)
+
+# Score
+score = 0
+high_score = 0
+results = open("Highest Game Scores.txt", "r+")
+
+# Set up the screen
+wn = turtle.Screen()
+wn.title("Snake Game by Pankti Patel")
+wn.bgcolor("pink")
+wn.bgpic('back 10.gif')
+wn.setup(width, height)
+wn.tracer(0) # Turns off the screen updates
+
+# Snake head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("black")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
+
+# Snake food
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("orange")
+food.penup()
+food.goto(0,100)
+
+segments = []
+
+# Snake toxic food
+food1 = turtle.Turtle()
+food1.speed(0)
+food1.shape("circle")
+food1.color("aqua")
+food1.penup()
+food1.goto(0,-100)
+
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("circle")
+pen.color("blue")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Score: 0  High Score: 0", align="center", font=("Playfair Display", 30, "normal"))
+
+# Functions
+def go_up():
+    #if head.direction != "down":
+        head.direction = "up"
+
+def go_down():
+    #if head.direction != "up":
+        head.direction = "down"
+
+def go_left():
+    #if head.direction != "right":
+        head.direction = "left"
+
+def go_right():
+    #if head.direction != "left":
+        head.direction = "right"
+
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+
+# Keyboard bindings
+wn.listen()
+wn.onkeypress(go_up, "Up")
+wn.onkeypress(go_down, "Down")
+wn.onkeypress(go_left, "Left")
+wn.onkeypress(go_right, "Right")
+
+# Main game loop
+while True:
+    wn.update()
+
+    # Check for a collision with the border
+    if head.xcor()>660 or head.xcor()<-650 or head.ycor()>330 or head.ycor()<-290:
+        time.sleep(1)
+        head.goto(0,0)
+        head.direction = "stop"
+
+        # Hide the segments
+        for segment in segments:
+            segment.goto(1000, 1000)
+        
+        # Clear the segments list
+        segments.clear()
+
+        # Reset the score
+        score = 0
+
+        # Reset the delay
+        delay = 0.1
+
+        pen.clear()
+        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Merriweather", 30, "normal")) 
+
+
+    # Check for a collision with the toxic food
+    if head.distance(food) < 20:
+        # Move the food to a random spot
+        x = random.randint(-290, 290)
+        y = random.randint(-290, 290)
+        food.goto(x,y)
+
+        # Add a segment
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("red")
+        new_segment.penup()
+        segments.append(new_segment)
+
+        # Shorten the delay
+        delay -= 0.001
+
+        # Increase the score
+        score += 10
+
+        if score > high_score:
+            high_score = score
+        
+        pen.clear()
+        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Merriweather", 30, "normal"))
+
+        # Check for a collision with the food
+    if head.distance(food1) < 20:
+        # Move the food to a random spot
+        x = random.randint(-290, 290)
+        y = random.randint(-290, 290)
+        food1.goto(x,y)
+        
+        # Remove a segment
+        if len(segments) > 0:
+             rem = segments[-1]
+             rem.hideturtle()
+             segments.remove(segments[-1])
+
+        # Shorten the delay
+        delay -= 0.001
+
+        # Increase the score
+        score -= 10
+        
+        pen.clear()
+        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Merriweather", 30, "normal")) 
+
+
+
+    # Move the end segments first in reverse order
+    for index in range(len(segments)-1, 0, -1):
+        x = segments[index-1].xcor()
+        y = segments[index-1].ycor()
+        segments[index].goto(x, y)
+
+    # Move segment 0 to where the head is
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x,y)
+
+    move()
+
+    if high_score >= 100:
+        wn.bgcolor("orange")
+
+    if high_score >= 150:
+        wn.bgcolor("yellow")
+
+    if high_score >= 200:
+        wn.bgcolor("green")
+
+    if high_score >= 250:
+        wn.bgcolor("blue")
+
+    if high_score >= 500:
+        wn.bgcolor("violet")
+
+
+    #Check for head collision with the body segments
+    for segment in segments:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0,0)
+            head.direction = "stop"
+        
+            # Hide the segments
+            for segment in segments:
+                segment.goto(1000, 1000)
+        
+            # Clear the segments list
+            segments.clear()
+
+            # Reset the score
+            score = 0
+
+            # Reset the delay
+            delay = 0.1
+        
+            # Update the score display
+            pen.clear()
+            pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
+
+    time.sleep(delay)
+
+wn.mainloop()
